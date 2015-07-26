5b13a74b5aba8648921818086c89f60d830d329e
Ziga Patacko Koderman <ziga.patacko@gmail.com>
2015-07-26 13:16:24 +0200

    js basics added - some bugs

--- index.js
+++ index.js
@@ -2,20 +2,64 @@
  * Created by zigapk on 7/17/15.
  */
 
-function clickHandler(e) {
-    var button = e.target;
-    while (!button.hasAttribute('data-dialog') && button !== document.body) {
-        button = button.parentElement;
+function refresh(e) {
+    //year and grade type choosers missing
+
+    var url = "http://127.0.0.1:8080?";
+    var endNeeded = false;
+
+    //grades
+    if(!document.getElementById("grade-1").hasAttribute("checked")) {
+        if(endNeeded) {
+            url += "&";
+            endNeeded = true;
+        }
+        url += "grade1=false"
+    }
+    if(!document.getElementById("grade-2").hasAttribute("checked")) {
+        if(endNeeded) {
+            url += "&";
+            endNeeded = true;
+        }
+        url += "grade2=false"
+    }
+    if(!document.getElementById("grade-3").hasAttribute("checked")) {
+        if(endNeeded) {
+            url += "&";
+            endNeeded = true;
+        }
+        url += "grade3=false"
+    }
+    if(!document.getElementById("grade-4").hasAttribute("checked")) {
+        if(endNeeded) {
+            url += "&";
+            endNeeded = true;
+        }
+        url += "grade4=false"
     }
 
-    if (!button.hasAttribute('data-dialog')) {
-        return;
+    //class chooser missing
+
+    //gender chooser
+    if(!document.getElementById("male-checkbox").hasAttribute("checked")) {
+        if(endNeeded) {
+            url += "&";
+            endNeeded = true;
+        }
+        url += "male=false"
+    }
+    if(!document.getElementById("male-checkbox").hasAttribute("checked")) {
+        if(endNeeded) {
+            url += "&";
+            endNeeded = true;
+        }
+        url += "female=false"
     }
 
-    var id = button.getAttribute('data-dialog');
-    var dialog = document.getElementById(id);
-    if (dialog) {
-        dialog.open();
-    }
+    $.getJSON(url, function(data){
+        //$('#output').html(data.contents);
+        alert(data["Facts"]["CurrentStudents"])
+    });
+
 }
 
